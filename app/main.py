7c5ac48ce98cdb1b56960f4dfcfeb3d9b74a88e8
badi-al-zaman <badi.al.zaman2446@gmail.com>
2025-11-06 04:31:14 -0800

    including the rag v2 controller

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ from app.core.config import settings as server_settings
 from app.core.db import init_db
 from app.utils.logger import logger
 from app.controllers.conversation_controller import session_router
+from app.controllers.rag_controller_v2 import router as rag_router_v2
 
 app = FastAPI(title=server_settings.PROJECT_NAME, description="Chat with multiple wiki articles :articles:")
 app.include_router(session_router, tags=["Sessions | Conversations"])
+app.include_router(rag_router_v2, prefix="/v2", tags=["Rag V2: Index, Search, Ask, Chat"])
 
 
 @app.on_event("startup")
