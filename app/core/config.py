ad502f91c0dbc547f26441874eea624dabadaff5
badi-al-zaman <badi.al.zaman2446@gmail.com>
2025-11-30 21:44:51 +0300

    core/auth: add SMTP variables along side with token vars too

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+import secrets
+import warnings
 from typing import Annotated, Any, Literal
 
 from pydantic import (
@@ -5,10 +7,11 @@ from pydantic import (
     BeforeValidator,
     PostgresDsn,
     computed_field,
-    EmailStr,
+    EmailStr, model_validator,
 )
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
+from typing_extensions import Self
 
 
 def parse_cors(v: Any) -> list[str] | str:
@@ -29,19 +32,46 @@ class Settings(BaseSettings):
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     PROJECT_NAME: str
-    POSTGRES_SERVER: str = "localhost"
-    POSTGRES_PORT: int = 5432
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
     POSTGRES_USER: str
-    POSTGRES_PASSWORD: str = ""
-    POSTGRES_DB: str = ""
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
-    FIRST_SUPERUSER: EmailStr = "badi.al.zaman@gmail.com"
-    FIRST_SUPERUSER_PASSWORD: str = "123456789"
+    FIRST_SUPERUSER: EmailStr
+    FIRST_SUPERUSER_PASSWORD: str
 
     API_V1_STR: str = "/api/v1"
-    FRONTEND_HOST: str = "http://localhost:5173"
+    FRONTEND_HOST: str
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
+    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
 
     DATA_DIR: str = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "articles")
+
+    SMTP_TLS: bool
+    SMTP_SSL: bool
+    SMTP_PORT: int
+    SMTP_HOST: str
+    SMTP_USER: str
+    SMTP_PASSWORD: str
+    EMAILS_FROM_EMAIL: EmailStr
+    EMAILS_FROM_NAME: str
+
+    @model_validator(mode="after")
+    def _set_default_emails_from(self) -> Self:
+        if not self.EMAILS_FROM_NAME:
+            self.EMAILS_FROM_NAME = self.PROJECT_NAME
+        return self
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def emails_enabled(self) -> bool:
+        return bool(self.SMTP_HOST and self.EMAILS_FROM_EMAIL)
 
     @computed_field  # type: ignore[prop-decorator]
     @property
@@ -77,6 +107,27 @@ class Settings(BaseSettings):
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
             self.FRONTEND_HOST
         ]
+
+    def _check_default_secret(self, var_name: str, value: str | None) -> None:
+        if value == "changethis":
+            message = (
+                f'The value of {var_name} is "changethis", '
+                "for security, please change it, at least for deployments."
+            )
+            if self.ENVIRONMENT == "local":
+                warnings.warn(message, stacklevel=1)
+            else:
+                raise ValueError(message)
+
+    @model_validator(mode="after")
+    def _enforce_non_default_secrets(self) -> Self:
+        self._check_default_secret("SECRET_KEY", self.SECRET_KEY)
+        self._check_default_secret("POSTGRES_PASSWORD", self.POSTGRES_PASSWORD)
+        self._check_default_secret(
+            "FIRST_SUPERUSER_PASSWORD", self.FIRST_SUPERUSER_PASSWORD
+        )
+
+        return self
 
 
 settings = Settings()  # type: ignore
